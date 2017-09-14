176b5a2ad860380edc9e16354b63b521faccdadb
l <llr.allleo@gmail.com>
2017-09-14 16:11:39 -0500

    updated readme file and config file
    updated readme file and config file

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ install node.js
 go to the application directory in cmd and set: npm install
 
 run the aplication: 
-npm index.js
+node index.js
 
 go to the application server as follow: http://localhost:3000/
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var configs = {};
 configs.applicationPort = 3000;
 configs.dbName = 'CrossoverVideosAssignment';
-configs.dbHost = 'DESKTOP-0A17EAM';
+configs.dbHost = 'localhost'; 
 
 module.exports = configs;
